708bf72caaab512d415488ae848962df66d52a41
Ivan <kattyrina77@gmail.com>
2022-02-17 22:41:58 +0300

    Add independent cell to the collection view Add new section to the collection Update collection view Add test object, just for making sure everything works fine

--- projectClimber/StatisticsCollectionViewController.swift
+++ projectClimber/StatisticsCollectionViewController.swift
@@ -11,8 +11,9 @@ import RealmSwift
 
 class StatisticsCollectionViewController: UICollectionViewController {
     
-    enum Section {
-        case main
+    enum Section: Hashable {
+        case history
+        case workouts
     }
     
     typealias DataSource = UICollectionViewDiffableDataSource<Section, Object>
@@ -21,36 +22,52 @@ class StatisticsCollectionViewController: UICollectionViewController {
     var workouts = [Object]()
     var dataSource: DataSource!
     var token: NotificationToken?
-    
+    // Array where kept sections
+    var sections = [Section]()
+    //TEST----
+    var history = History()
+    //--------
     override func viewDidLoad() {
         super.viewDidLoad()
         print("STATISTICS COLLECTION VIEW - VIEW DID LOAD")
         self.collectionView!.register(WorkoutCollectionViewCell.self, forCellWithReuseIdentifier: WorkoutCollectionViewCell.reuseIdentifier)
+        self.collectionView!.register(HistoryCollectionViewCell.self, forCellWithReuseIdentifier: HistoryCollectionViewCell.reuseIdentifier)
+
+        collectionView.collectionViewLayout = createLayout()
         workouts = RealmManager.sharedInstance.fetch(object: FingerWorkout.self)
         observeRealm()
         viewConfiguration()
         configureDataSource()
-        collectionView.setCollectionViewLayout(createLayout(), animated: true)
+        
+        print(sections)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("STATISTICS COLLECTION VIEW - VIEW DID APPEAR")
         var snapshot = Snapshot()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(self.workouts, toSection: .main)
+        snapshot.appendSections([.history])
+        snapshot.appendItems([history], toSection: .history)
+        
+        snapshot.appendSections([.workouts])
+        snapshot.appendItems(self.workouts, toSection: .workouts)
         self.dataSource.apply(snapshot)
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // Move to statistics table view
-        let workoutToShow = workouts[indexPath.item]
-        let tableStatView = WorkoutStatisticsTableViewController()
-        tableStatView.workout = workoutToShow as? FingerWorkout
-        // Prevent table view to have a large title
-        tableStatView.navigationItem.largeTitleDisplayMode = .never
-        
-        navigationController?.pushViewController(tableStatView, animated: true)
+        let section = sections[indexPath.section]
+        // If section is .workouts we can push to stats for specific workout
+        if section == .workouts {
+            // Move to statistics table view
+            let workoutToShow = workouts[indexPath.item]
+            let tableStatView = WorkoutStatisticsTableViewController()
+            tableStatView.workout = workoutToShow as? FingerWorkout
+            // Prevent table view to have a large title
+            tableStatView.navigationItem.largeTitleDisplayMode = .never
+            
+            navigationController?.pushViewController(tableStatView, animated: true)
+        }
+        // ... otherwise nothing happens
     }
     
     func viewConfiguration() {
@@ -60,22 +77,50 @@ class StatisticsCollectionViewController: UICollectionViewController {
     }
     
     func createLayout() -> UICollectionViewLayout {
-        let padding: CGFloat = 10
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalWidth(0.5))
         
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.5))
-        
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
-        
-        group.interItemSpacing = .fixed(padding * 2)
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment -> NSCollectionLayoutSection? in
+            let section = self.sections[sectionIndex]
+            // Switch between different sections
+            // And create theirown layout for particular section
+            switch section {
+            case .history:
+                let padding: CGFloat = 10
                 
-        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: padding, bottom: 0, trailing: padding)
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.4))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.4))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+                
+                group.interItemSpacing = .fixed(padding * 2)
+                
+                group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: padding, bottom: 0, trailing: padding)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                return section
+                
+            case .workouts:
+                let padding: CGFloat = 10
+                
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.2))
+                
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.2))
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+                
+                group.interItemSpacing = .fixed(padding * 2)
+                        
+                group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: padding, bottom: 0, trailing: padding)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                return section
+            }
+        }
+        return layout
         
-        let section = NSCollectionLayoutSection(group: group)
-        
-        return UICollectionViewCompositionalLayout(section: section)
     }
     
     func observeRealm() {
@@ -88,19 +133,43 @@ class StatisticsCollectionViewController: UICollectionViewController {
     //MARK: Data Source configuring
     func configureDataSource() {
         dataSource = .init(collectionView: collectionView, cellProvider: { collectionView, indexPath, workout -> UICollectionViewCell? in
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WorkoutCollectionViewCell.reuseIdentifier, for: indexPath) as! WorkoutCollectionViewCell
-            
-            cell.configure(with: workout)
-            cell.layer.cornerRadius = 10
-            cell.backgroundColor = .systemOrange
-            return cell
-            
+            let section = self.sections[indexPath.section]
+            // Switch between sections
+            // Configuring cell depends on section
+            switch section {
+            case .history:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HistoryCollectionViewCell.reuseIdentifier, for: indexPath) as! HistoryCollectionViewCell
+                
+                cell.configure(with: self.history)
+                cell.layer.cornerRadius = 10
+                cell.backgroundColor = .systemFill
+                
+                return cell
+            case .workouts:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WorkoutCollectionViewCell.reuseIdentifier, for: indexPath) as! WorkoutCollectionViewCell
+                
+                cell.configure(with: workout)
+                cell.layer.cornerRadius = 10
+                cell.backgroundColor = .systemOrange
+                
+                return cell
+            }
         })
-        //where I need to safe my workouts?????
-        //temporarly I save them in this view controller
+
         var snapshot = Snapshot()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(workouts, toSection: .main)
+        // add each seciton to the snapshot
+        snapshot.appendSections([.history])
+        snapshot.appendItems([history], toSection: .history)
+        
+        snapshot.appendSections([.workouts])
+        snapshot.appendItems(workouts, toSection: .workouts)
+        // asign sections to the value of snapshot section identifiers
+        sections = snapshot.sectionIdentifiers
         dataSource.apply(snapshot)
     }
+}
+// TEST-----
+// Class just for making sure that cell appears properly
+class History: Object {
+    @Persisted var totalTime = 400
 }
